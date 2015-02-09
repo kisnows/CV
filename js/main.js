6ce3fe185f49cf133d280e68c0831647f268fdc5
YuanQi <yq12315@gmail.com>
2015-02-09 23:10:22 +0800

    chang background color

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('#pagepiling').pagepiling({
         scrollingSpeed: 10,
         loopBottom: true,
         css3: true,
-        anchors: ['首页','项目经历','个人技能','教育背景','联系方式'],
-        navigation:{
+        anchors: ['首页', '项目经历', '个人技能', '教育背景', '联系方式'],
+        navigation: {
             'position': 'right',
-            'tooltips':['首页','项目经历','个人技能','教育背景','联系方式']
+            'tooltips': ['首页', '项目经历', '个人技能', '教育背景', '联系方式']
+        },
+        onLeave: function(index, nextIndex, direction) {
+            var $pages = $('.section');
+            for (var i = 0,j = $pages.length; i < j; i++) {
+                if (i + 1 !== nextIndex) {
+                    $($pages[i]).css('background-color', '#FACF44');
+                } else {
+                    $($pages[i]).css('background-color', '#DC3955');
+                }
+            }
         }
     });
     $(window).bind('mousewheel', function() {
@@ -14,17 +24,15 @@ $(document).ready(function(){
         if ($('.page3').hasClass('active')) {
             console.log(tags)
             for (var i = tags.length - 1; i >= 0; i--) {
-                $(tags[i]).animate(
-                    {
-                        padding: '0 .8em'
-                    }, 500);
+                $(tags[i]).animate({
+                    padding: '0 .8em'
+                }, 500);
             }
-        }else{
+        } else {
             for (var j = tags.length - 1; j >= 0; j--) {
-                $(tags[j]).animate(
-                    {
-                        padding: '0 .3em'
-                    }, 300);
+                $(tags[j]).animate({
+                    padding: '0 .3em'
+                }, 300);
             }
         }
     });
